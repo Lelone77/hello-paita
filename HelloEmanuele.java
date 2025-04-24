1e67c0064b249fc1c44a102cde11c04ac3003456
Lelone77 <emanuelepaita@capellinisauro.edu.it>
2025-04-24 07:14:44 +0000

    stampa 3 volte il nome

--- HelloEmanuele.java
+++ HelloEmanuele.java
@@ -3,5 +3,7 @@ import java.io.*;
 public class HelloEmanuele {
 public static void main(String[] args) {
 System.out.println("Hello Emanuele !");
+System.out.println("Hello Emanuele !");
+System.out.println("Hello Emanuele !");
 }
 }
