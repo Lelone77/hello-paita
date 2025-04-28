fa8eaca1065648f441c8db4f07fdde67a9a8e67f
Lelone77 <emanuelepaita@capellinisauro.edu.it>
2025-04-28 16:09:26 +0000

    stampa Hello nome

--- HelloEmanuele.java
+++ HelloEmanuele.java
@@ -2,6 +2,6 @@ import java.io.*;
 
 public class HelloEmanuele {
 public static void main(String[] args) {
-System.out.println("Forza Napoli!");
+System.out.println("Forza Emanuele!"); //stampa Hello nome
 }
 }
