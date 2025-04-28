e4c0edf2b096106ef85c12e84fadbc2b95318862
Lelone77 <emanuelepaita@capellinisauro.edu.it>
2025-04-28 16:12:41 +0000

    stampa squadra del cuore

--- HelloEmanuele.java
+++ HelloEmanuele.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 System.out.println("Hello Emanuele!"); //stampa Hello nome
 
 System.out.println("19/05/2007!"); //stampa Data di nascita
-}
+
+System.out.println("Forza Napoli!"); //stampa squadra del cuore
 }
