f32b72dd3dab42c2a394fed80305cef4dfe352fe
Lelone77 <emanuelepaita@capellinisauro.edu.it>
2025-04-30 09:22:25 +0000

    Stampa cibo preferito

--- HelloEmanuele.java
+++ HelloEmanuele.java
@@ -7,4 +7,6 @@ System.out.println("Hello Emanuele!"); //stampa Hello nome
 System.out.println("19/05/2007!"); //stampa Data di nascita
 
 System.out.println("Forza Napoli!"); //stampa squadra del cuore
+
+System.out.println("Pizza!"); //stampa cibo preferito
 }
